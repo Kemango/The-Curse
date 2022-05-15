09f747036fa8a4c58d8364175b05beae5bb1bbc0
Kemango <guokeming007@hotmail.com>
2022-05-15 20:04:35 +0800

    menu update

---
+++ Assets/Script/MenuManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+	public void StartGame ()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+	}
+
+	public void QuitGame()
+	{
+		Debug.Log("WE QUIT THE GAME!");
+		Application.Quit();
+	}   
+}
